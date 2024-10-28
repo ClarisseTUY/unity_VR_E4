fa9dd2a78dfa74b88e688de11fd47e6bc393fcfe
ClarisseTUY <clarisse.tuy@edu.esiee.fr>
2024-10-28 18:36:11 +0100

    pointing at item

---
+++ VRUnityProjet/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    public GameObject interaction_Info_UI;
+    public TMP_Text interaction_text;
+
+    private void Start()
+    {
+        interaction_text = interaction_Info_UI.GetComponent<TMP_Text>();
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selectionTransform = hit.transform;
+
+            if (selectionTransform.GetComponent<InteractableObject>() && selectionTransform.GetComponent<InteractableObject>().playerInRange)
+            {
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
+            }
+            else
+            {
+                interaction_Info_UI.SetActive(false);
+            }
+
+        }
+        else
+        {
+            interaction_Info_UI.SetActive(false);
+        }
+    }
+}
