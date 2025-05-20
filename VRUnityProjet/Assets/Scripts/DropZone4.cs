17905ffd6d7dbb0ffe0e5397a8f594c99c51ac99
julie-tssn <julie.tesson@edu.esiee.fr>
2025-05-20 22:43:28 +0200

    test

---
+++ VRUnityProjet/Assets/Scripts/DropZone4.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class DropZone4 : MonoBehaviour
+{
+    public GameObject objectToActivate;
+    public Material newMaterial;
+
+    public string expectedTag = "probe";
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (!other.CompareTag(expectedTag))
+        {
+            Debug.Log("Objet ignoré : " + other.name + " (tag = " + other.tag + ")");
+            return;
+        }
+
+
+        if (objectToActivate != null)
+        {
+            objectToActivate.SetActive(true);
+        }
+
+
+        Renderer zoneRenderer = GetComponent<Renderer>();
+        if (zoneRenderer != null && newMaterial != null)
+        {
+            zoneRenderer.material = newMaterial;
+        }
+        else
+        {
+            Debug.LogWarning("DropZone n'a pas de Renderer ou de Matériau assigné !");
+        }
+    }
+}
